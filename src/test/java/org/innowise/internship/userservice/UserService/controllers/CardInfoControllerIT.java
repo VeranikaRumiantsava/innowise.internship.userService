4f8c6ac44b119edeb243603dc55fc72b4bb127b7
rumronya <rum.ronya@gmail.com>
2025-07-23 15:45:49 +0300

    try jacoco 2.0

--- src/test/java/org/innowise/internship/userservice/UserService/controllers/CardInfoControllerIT.java
+++ src/test/java/org/innowise/internship/userservice/UserService/controllers/CardInfoControllerIT.java
@@ -27,7 +27,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
+@SpringBootTest
+@Testcontainers
 @ActiveProfiles("test")
 public class CardInfoControllerIT extends BaseIT {
 
